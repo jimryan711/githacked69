39fb83013665d9913c5f91d20529e2414b8457ec
githacked69 <jimryan711@gmail.com>
2018-12-08 14:27:16 -0500

    adding server `# Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => res.send('Jamie likes boyz'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 4000,, () => console.log(`Example app listening on port ${port}!`))
