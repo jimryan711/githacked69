55b22b3fb1df8ab429c153f8f81bec7ede952a75
githacked69 <jimryan711@gmail.com>
2018-12-08 14:34:24 -0500

    d

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Jamie likes boyz'))
 
-app.listen(process.env.PORT || 4000, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 4000, () => console.log(`Example app listening on port!`))
