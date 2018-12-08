7adca134feb23dc85c7e742d9c1da21e19e628c2
githacked69 <jimryan711@gmail.com>
2018-12-08 16:54:52 -0500

    adding

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Jamie likes gai boyz'))
+// app.get('/', (req, res) => res.send('Jamie likes gai boyz'))
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || 4000, () => console.log(`Example app listening on port!`))
